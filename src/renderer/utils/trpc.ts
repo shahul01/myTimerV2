1d0a52e71bb3c7e14caad28e8f9249c15051a882
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2023-08-07 02:54:24 +0530

    refactor: moved index and trpc file in /server to /server/api

--- src/renderer/utils/trpc.ts
+++ src/renderer/utils/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../server';
+import type { AppRouter } from '../../server/api';
 
 // eslint-disable-next-line import/prefer-default-export
 export const trpc = createTRPCReact<AppRouter>();
